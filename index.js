be5e4c678281c1d11456b8f4d21dbc78ca7244c8
Brian J Brennan <brianloveswords@gmail.com>
2016-07-29 03:06:57 -0400

    checkpoint

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function clickHandler(event) {
   // script.innerHTML = this.innerText;
   // document.body.appendChild(script);
 
-  eval(this.innerText);
+  window.eval(this.innerText);
 
   return false;
 }
@@ -50,9 +50,37 @@ function clickHandler(event) {
 function hookUpEventListeners() {
   let elements = Array.from(document.querySelectorAll('pre > code'));
   elements.forEach(el => {
+    el.spellcheck = false;
     el.addEventListener('click', clickHandler);
   });
 }
 
 // loadPage();
 hookUpEventListeners();
+
+
+// class Album {
+//   constructor(artist, album, year) {
+//     this.artist = artist;
+//     this.album = album;
+//     this.year = year;
+//   }
+// }
+
+// let albums = [
+//   new Album('Stove', 'Is Stupider', 2015),
+//   new Album('PJ Harvey', 'Rid Of Me', 1992),
+//   new Album('Cloakroom', 'Further Out', 2015),
+//   new Album('Television', 'Marquee Moon', 1997),
+//   new Album('Noveller', 'No Dream', 2013),
+//   new Album('Gary Numan', 'The Pleasure Principle', 1979),
+//   new Album('Dismemberment Plan', 'Change', 2001),
+//   new Album('Slint', 'Spiderland', 1991),
+//   new Album('Chavez', 'Ride the Fader', 1996),
+//   new Album('The Psychic Paramount',
+//             'Gamelan Into the Mink Supernatural', 2005),
+//   new Album('Sharks Keep Moving', 'Desert Strings and Drifers', 1999),
+//   new Album("Swearin'", 'Surfing Strange', 2013),
+//   new Album('The Verve', 'Storm In Heaven', 2008),
+//   new Album('Ringo Deathstarr', 'Colour Trip', 2011),
+// ];
